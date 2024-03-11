b2a30db8b2468437dd867e257f00284121b515da
Ajay Joy <ajayjoy618@gmail.com>
2024-03-11 14:17:17 +0530

    Banner Componet

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons'>
+               <button className='button'>Play</button>
+               <button className='button'>My List</button>
+            </div>
+            <h1 className='describtion'>Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan.</h1>
+        </div> 
+        <div className="fade_bottom">
+        </div>    
+    </div>
+  )
+}
+
+export default Banner
