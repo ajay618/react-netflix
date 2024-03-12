3d103fa9bf343e14fbbaa98a0ded366f561a646d
Ajay Joy <ajayjoy618@gmail.com>
2024-03-12 13:16:34 +0530

    banner image loading from Api

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,23 +1,28 @@
-import React, { useEffect } from 'react'
-import {API_KEY} from '../../Constants/Constants'
+import React, { useEffect ,useState} from 'react'
+import {API_KEY , imageUrl} from '../../Constants/Constants'
 import './Banner.css'
 import axios from '../../axios'
+
 function Banner() {
+  const [movie, setMovie] = useState([])
   useEffect(() => {
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) =>{
-      console.log(response.data.results[0]);
+      const randomIndex = Math.floor(Math.random() * response.data.results.length);
+      setMovie(response.data.results[randomIndex])
       });
   }, []);
   
   return (
-    <div className='banner'>
+    <div
+    style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})`}} 
+    className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie Name</h1>
+        <h1 className='title'>{movie ? (movie.title || movie.name || ' ') : ' '}</h1>
             <div className='banner_buttons'>
                <button className='button'>Play</button>
                <button className='button'>My List</button>
             </div>
-            <h1 className='describtion'>Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan.</h1>
+            <h1 className='describtion'>{movie ? movie.overview : ' '}</h1>
         </div> 
         <div className="fade_bottom">
         </div>    
--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,2 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
 export const API_KEY = '2dbb6be163637374d91ca91d6aa467e4';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
